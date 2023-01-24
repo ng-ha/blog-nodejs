5dfe6d70b2f1f2401976546d0fb89c00f82de84b
nguyenha1111 <nguyenthanhha.html@gmail.com>
2023-01-24 11:26:55 +0700

    use static, install node-sass

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "/public")));
+
 // HTTP logger
 app.use(morgan("dev"));
 
