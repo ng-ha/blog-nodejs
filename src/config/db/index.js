d58adf88321f6209306a5deaedefc8940112c95b
ng-ha <ngthanhha.hn@gmail.com>
2023-05-11 08:57:38 +0700

    update connection to MongoDB Atlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,19 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 async function connect() {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/education_dev");
-        console.log("Mongoose: Connect SUCCESSFULLY!!!");
-    } catch (error) {
-        console.log("Mongoose: Connect FAILED!!!");
-    }
+  try {
+    // await mongoose.connect("mongodb://127.0.0.1:27017/education_dev");
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@blognode.1cpegiz.mongodb.net/Blognode?retryWrites=true&w=majority`
+    );
+    console.log('Mongoose: Connect SUCCESSFULLY!!!');
+  } catch (error) {
+    console.log('Mongoose: Connect FAILED!!!');
+  }
 }
 
 // [MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back
 // to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want
 // to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 module.exports = { connect };
-
-// config/db là function dùng để connect tới db
--- src/index.js
+++ src/index.js
@@ -1,62 +1,56 @@
-// CommonJS
-const path = require("path");
-const express = require("express");
-const handlebars = require("express-handlebars");
-const morgan = require("morgan");
-const methodOverride = require("method-override");
-const sortMiddleWare = require("./app/middlewares/sortMiddleware");
-
-// import path from "path";
-// import express from "express";
-// import handlebars from "express-handlebars";
-// import morgan from "morgan";
-// dùng import khi thêm "type": "module" trong package.json => ES module
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const sortMiddleWare = require('./app/middlewares/sortMiddleware');
 
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
 
-const db = require("./config/db");
+const db = require('./config/db');
 // connect to DB
 db.connect();
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // use middleware
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(express.json());
 
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
 app.use(sortMiddleWare);
 
 // HTTP logger
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 // Template engine
 app.engine(
-    "hbs",
-    handlebars.engine({
-        extname: ".hbs",
-        helpers: require("./helpers/handlebars"),
-    })
+  'hbs',
+  handlebars.engine({
+    extname: '.hbs',
+    helpers: require('./helpers/handlebars'),
+  })
 );
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 
 // c1:
 // app.set("views", "./src/resources/views");
 // c2:
-app.set("views", path.join(__dirname, "resources", "views"));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // route
 route(app);
 
 // 127.0.0.1 - localhost
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
